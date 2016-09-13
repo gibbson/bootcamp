ba1dbdb299dc2a8a1eb53df39e3d4c1fd469ad0a
黄山 <shuang3@caltech.edu>
2016-09-13 11:32:34 -0700

    A debug

--- thirdday.py
+++ thirdday.py
@@ -1,1 +1,15 @@
 import numpy as np
+
+xa_high = np.loadtxt('data/xa_high_food.csv', )
+xa_low = np.loadtxt('data/xa_low_food.csv', )
+
+def xa_high_diaameter(xa):
+    """
+    Convert an array of cross_sectional areas to diameters with commuensurate units
+    """
+
+    # Compute diameter from area
+    # A = pi * d^2 / 4
+    diameter = np.sqrt(xa * 4 / np.pi)
+    
+    return diameter
